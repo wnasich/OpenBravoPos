706a9cd5412057deab6549951ba562f8f5e6899b
Walter Nasich <wnasich@gmail.com>
2015-09-02 11:41:55 -0300

    Fixing typo

--- resources/limpieza_db_delete.sql
+++ resources/limpieza_db_delete.sql
@@ -6,7 +6,7 @@ delete from CLOSEDCASH where CLOSEDCASH.MONEY in (select RECEIPTS.MONEY from REC
 select ROW_COUNT() as closedcash_deleted;
 delete from PAYMENTS where PAYMENTS.RECEIPT in (select RECEIPTS.ID from RECEIPTS where RECEIPTS.DATENEW between @date_from and @date_to);
 select ROW_COUNT() as payments_deleted;
-delete from TAXLINES where TAXLINES.RECEIPT in (selecct RECEIPTS.ID from RECEIPTS where RECEIPTS.DATENEW between @date_from and @date_to);
+delete from TAXLINES where TAXLINES.RECEIPT in (select RECEIPTS.ID from RECEIPTS where RECEIPTS.DATENEW between @date_from and @date_to);
 select ROW_COUNT() as taxlines_deleted;
 delete from TICKETLINES where TICKETLINES.TICKET in (select TICKETS.ID from TICKETS inner join RECEIPTS ON TICKETS.ID=RECEIPTS.ID where RECEIPTS.DATENEW between @date_from and @date_to);
 select ROW_COUNT() as ticketlines_deleted;
