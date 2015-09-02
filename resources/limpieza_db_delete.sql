b1f36c3f28ab121b7833d7c2206b0802536e769a
Walter Nasich <wnasich@gmail.com>
2015-09-02 20:32:07 -0300

    Execution order

--- resources/limpieza_db_delete.sql
+++ resources/limpieza_db_delete.sql
@@ -2,8 +2,6 @@ SET @date_from = '2000-01-01 00:00:00';
 SET @date_to = '2014-12-31 23:59:59';
 
 select @date_from, @date_to;
-delete from CLOSEDCASH where CLOSEDCASH.MONEY in (select RECEIPTS.MONEY from RECEIPTS where RECEIPTS.DATENEW between @date_from and @date_to);
-select ROW_COUNT() as closedcash_deleted;
 delete from PAYMENTS where PAYMENTS.RECEIPT in (select RECEIPTS.ID from RECEIPTS where RECEIPTS.DATENEW between @date_from and @date_to);
 select ROW_COUNT() as payments_deleted;
 delete from TAXLINES where TAXLINES.RECEIPT in (select RECEIPTS.ID from RECEIPTS where RECEIPTS.DATENEW between @date_from and @date_to);
@@ -16,3 +14,5 @@ delete from TICKETS where TICKETS.ID in (select RECEIPTS.ID from RECEIPTS where 
 select ROW_COUNT() as tickets_deleted;
 delete from RECEIPTS where DATENEW between @date_from and @date_to;
 select ROW_COUNT() as receipts_deleted;
+delete from CLOSEDCASH where CLOSEDCASH.MONEY not in (select RECEIPTS.MONEY from RECEIPTS);
+select ROW_COUNT() as closedcash_deleted;
